618a77cb1e4f9b1088ba36dd6836db7383f1a0e8
Shonn-Shonn <sshonn687@gmail.com>
2023-08-29 16:50:03 +0630

    test: attribute and checkbox

--- vue/malar-spicy/src/App.vue
+++ vue/malar-spicy/src/App.vue
@@ -1,47 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+  export default {
+  data() {
+    return {
+      rawHtml : '<span style="color: red">This should be red.</span>',
+      link: 'https://www.google.com',
+      isChecked: false,
+    };
+  }
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p>Using text interpolation: {{ rawHtml }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+  <a v-bind:href="link">www.google.com</a>
+  <input type="checkbox" :checked="isChecked">
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>

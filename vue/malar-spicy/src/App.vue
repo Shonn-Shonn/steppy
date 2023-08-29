ad24f842cf33a9621fc341a7bb43696fcad4dd1b
Shonn-Shonn <sshonn687@gmail.com>
2023-08-29 20:03:48 +0630

    add: v-html and v-bind in App.vue

--- vue/malar-spicy/src/App.vue
+++ vue/malar-spicy/src/App.vue
@@ -1,10 +1,23 @@
 <script>
+  const objectOfAttrs = {
+  id: 'container',
+  class: 'wrapper'
+  }
+
   export default {
   data() {
     return {
       rawHtml : '<span style="color: red">This should be red.</span>',
       link: 'https://www.google.com',
       isChecked: false,
+      objectOfAttrs : {
+            id: 'container',
+            class: 'wrapper'
+      },
+      number: 1,
+      ok: true,
+      message: 'Hello, Vue!',
+      id: 1,
     };
   }
 };
@@ -13,10 +26,23 @@
 <template>
   <p>Using text interpolation: {{ rawHtml }}</p>
   <p>Using v-html directive: <span v-html="rawHtml"></span></p>
-  <a v-bind:href="link">www.google.com</a>
+  <a :href="link">www.google.com</a>
   <input type="checkbox" :checked="isChecked">
+
+  <div>
+    <p>Number + 1 : {{number + 1}}</p>
+    <p>{{ok ? 'YES' : 'No'}}</p>
+    <p>Reversed Message: {{message.split('').reverse().join('')}}</p>
+    <div :id="`list-${id}`">Dynamic ID: {{id}}</div>
+  </div>
+
+  <div class="ai" v-bind="objectOfAttrs">
+    {{ objectOfAttrs.class }}
+  </div>
 </template>
 
 <style scoped>
 
 </style>
+
+//v-bind 

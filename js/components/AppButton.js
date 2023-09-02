b8155cda55e6d29be073a9119f584ba0a11781e6
Shonn-Shonn <sshonn687@gmail.com>
2023-09-02 09:01:21 +0630

    bug: button

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,8 +1,17 @@
 export default {
   props: ["isButtonDisabled", "isChosen"],
   template: `
-    <button class="bg-gray-200 hover:bg-gray-400 border-rounded px-5 py-2">
+    <button class="{
+      'bg-gray-200 hover:bg-gray-400 border-rounded px-5 py-2:false':true
+    }">
         <slot/>
     </button>
     `,
+
+  props: {
+    type: {
+      type: String,
+      default: primary,
+    },
+  },
 };

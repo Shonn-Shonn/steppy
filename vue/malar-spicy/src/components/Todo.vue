1ffc70f54ea71b7c77fbef4f27f484ec696e93b0
Shonn-Shonn <sshonn687@gmail.com>
2023-09-04 23:21:49 +0630

    add: todolist

---
+++ vue/malar-spicy/src/components/Todo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a new todo" />
+      <ul>
+        <li v-for="(todo, index) in todos" :key="index">
+          {{ todo }}
+          <button @click="removeTodo(index)">Remove</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    setup() {
+      const newTodo = ref('');
+      const todos = ref([]);
+  
+      const addTodo = () => {
+        if (newTodo.value.trim() !== '') {
+          todos.value.push(newTodo.value);
+          newTodo.value = '';
+        }
+      };
+  
+      const removeTodo = (index) => {
+        todos.value.splice(index, 1);
+      };
+  
+      return {
+        newTodo,
+        todos,
+        addTodo,
+        removeTodo,
+      };
+    },
+  };
+  </script>
+  
